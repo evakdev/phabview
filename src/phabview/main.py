8823f09a2ff0abcc44dfc449fc186062b74978c6
evakdev <ivakdad@gmail.com>
2024-06-17 02:08:37 +0330

    Separate main functions from webhook view

--- src/phabview/main.py
+++ src/phabview/main.py
@@ -1,57 +1,19 @@
-import hashlib
-import hmac
 from http import HTTPStatus
 from asgiref.wsgi import WsgiToAsgi
-from phabview.config import (
-    NOTIFIABLE_USERS,
-    NOTIFY_ANY_USER,
-    PHABRICATOR_HMAC_HEADER_NAME,
-    PHABRICATOR_REVISION_TYPE_NAME,
-    PHABRICATOR_WEBHOOK_HMAC_KEY,
-)
+from phabview.config import PHABRICATOR_HMAC_HEADER_NAME
 from flask import Flask, Response, request
-
-from phabview.adapters.adapter_factory import AdapterFactory
-from phabview.phab.update_manager import UpdateManager
+from phabview.phabview import PhabView
 
 app = Flask("PhabView")
 
 
 @app.route("/receive_webhook", methods=["POST"])
 async def receive_webhook():
-    # hmac_object = hmac.new(
-    #     key=bytes(PHABRICATOR_WEBHOOK_HMAC_KEY, "utf-8"),
-    #     msg=request.data,
-    #     digestmod=hashlib.sha256,
-    # )
-    # signature = hmac_object.hexdigest()
-    # hmac_header = request.headers.get(PHABRICATOR_HMAC_HEADER_NAME)
-    # if hmac_header != signature:
-    #     raise Exception
-    data = request.json
-    changed_object = data["object"]
+    phabview = PhabView()
+    hmac_header = request.headers.get(PHABRICATOR_HMAC_HEADER_NAME)
+    phabview.verify_hmac(request_data=request.data, hmac_header=hmac_header)
+    await phabview.handle(request.json)
 
-    if changed_object["type"] != PHABRICATOR_REVISION_TYPE_NAME:
-        return Response(status=HTTPStatus.OK)
-
-    transactions = []
-    for transaction in data["transactions"]:
-        transactions.extend(transaction.values())
-
-    phabricator_update_manager = UpdateManager()
-    update = phabricator_update_manager.get_update(changed_object, transactions)
-    if not update:
-        return Response(status=HTTPStatus.OK)
-    notifications = phabricator_update_manager.create_notifs(update)
-    messaging_adapter = AdapterFactory().get_adapter()
-    if NOTIFY_ANY_USER:
-        for notification in notifications:
-            print(f'to {notification["user"]}: {notification["text"]}')
-            # await messaging_adapter.send(notification["user"], notification["text"])
-        return Response(status=HTTPStatus.OK)
-    for notification in notifications:
-        if notification["user"] in NOTIFIABLE_USERS:
-            await messaging_adapter.send(notification["user"], notification["text"])
     return Response(status=HTTPStatus.OK)
 
 
---
+++ src/phabview/phabview.py
@@ -0,0 +1,55 @@
+import hashlib
+import hmac
+from phabview.config import (
+    PHABRICATOR_REVISION_TYPE_NAME,
+    PHABRICATOR_WEBHOOK_HMAC_KEY,
+    NOTIFY_ANY_USER,
+    NOTIFIABLE_USERS,
+)
+from phabview.adapters.adapter_factory import AdapterFactory
+from phabview.phab.update_manager import UpdateManager
+
+
+class PhabView:
+    def verify_hmac(self, request_data: bytes, hmac_header: str):
+        hmac_object = hmac.new(
+            key=bytes(PHABRICATOR_WEBHOOK_HMAC_KEY, "utf-8"),
+            msg=request_data,
+            digestmod=hashlib.sha256,
+        )
+        signature = hmac_object.hexdigest()
+        if hmac_header != signature:
+            raise Exception
+
+    async def handle(self, webhook_data: dict):
+        changed_object = webhook_data["object"]
+
+        if changed_object["type"] != PHABRICATOR_REVISION_TYPE_NAME:
+            return
+
+        raw_transactions = self._get_raw_transactions(webhook_data)
+        phabricator_update_manager = UpdateManager()
+        update = phabricator_update_manager.get_update(changed_object, raw_transactions)
+        if not update:
+            return
+
+        notifications = phabricator_update_manager.create_notifs(update)
+        await self.send_notifications(notifications)
+
+    async def send_notifications(self, notifications):
+        messaging_adapter = AdapterFactory().get_adapter()
+
+        if NOTIFY_ANY_USER:
+            for notification in notifications:
+                print(f'to {notification["username"]}: {notification["text"]}')
+                # await messaging_adapter.send(notification["user"], notification["text"])
+            return
+        for notification in notifications:
+            if notification["username"] in NOTIFIABLE_USERS:
+                await messaging_adapter.send(notification["username"], notification["text"])
+
+    def _get_raw_transactions(self, webhook_data):
+        transactions = []
+        for transaction in webhook_data["transactions"]:
+            transactions.extend(transaction.values())
+        return transactions
