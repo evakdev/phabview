a64d9435dd8869349d68dd138197ba5f99b4f168
evakdev <ivakdad@gmail.com>
2024-07-02 15:47:23 +0330

    Use retry_async instead of sync

--- src/phabview/adapters/rocketchat.py
+++ src/phabview/adapters/rocketchat.py
@@ -1,6 +1,6 @@
 from phabview.adapters.base_adapter import BaseMessagingAdapter
 from phabview.config import ROCKETCHAT_HOST, ROCKETCHAT_USER_ID, ROCKET_CHAT_USER_TOKEN, ROCKET_CHAT_BOT_ALIAS_NAME
-from retry import retry
+import retry_async
 from rocketchat_API.rocketchat import RocketChat
 
 
@@ -12,7 +12,7 @@ class RocketChatAdapter(BaseMessagingAdapter):
             server_url=ROCKETCHAT_HOST,
         )
 
-    @retry(delay=10, tries=10, backoff=2)
+    @retry_async.retry(is_async=True, tries=3)(delay=10, tries=10, backoff=2)
     async def send_to_user_dm(self, username: str, message: str):
         alias = ROCKET_CHAT_BOT_ALIAS_NAME or None
         response = self.api.chat_post_message(text=message, channel=f"@{username}", alias=alias)
